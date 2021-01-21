5e4e01542fe0d912ea18edddcd5fb21ec597ed4b
Masha A <mabramovaa01@gmail.com>
2021-01-20 19:21:17 -0500

    hi

---
+++ app.js
@@ -0,0 +1,38 @@
+var data = require("./data.json") 
+var express = require("express");
+const { query } = require('express');
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+//load   results from               
+
+    app.get("/all", (req, res) => {
+    res.json(data);
+   });
+
+   app.get("/planets", (req, res) => {
+    res.json(data.planets);
+   });
+
+   app.get("/moons", (req, res) => {
+    res.json(data.moons);
+   });
+
+   app.get("/stars", (req, res) => {
+    res.json(data.stars);
+   });
+
+   app.get("/exoplanets", (req, res) => {
+    res.json(data.exoplanets);
+   });
+
+
+   app.get("/galaxies", (req, res) => {
+    res.json(data.galaxies);
+   });
+
+
+   //all bodies in one request
+   //
